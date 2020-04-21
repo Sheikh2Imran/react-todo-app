5b05c1adfcf61913583df6ab1ff85256216d8404
Sheikh2Imran <imranjustcse@gmail.com>
2020-04-21 23:00:30 +0600

    local storage is added

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -2,12 +2,12 @@ import React, { useContext, useState } from 'react';
 import { BookContext } from '../contexts/BookContext';
 
 const AddBook = () => {
-    const { addNewBook } = useContext(BookContext);
+    const { dispatch } = useContext(BookContext);
     const [title, setTitle] = useState('');
     const [author, setAuthor] = useState('');
     const handleSubmit = (e) => {
         e.preventDefault();
-        addNewBook(title, author);
+        dispatch({type: 'ADD_BOOK', book: {title, author}});
         setTitle('');
         setAuthor('');
 
--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,24 +1,19 @@
-import React, { createContext, useState } from 'react';
-import uuid from 'uuid/v1';
+import React, { createContext, useReducer, useEffect } from 'react';
+import bookReducer from '../reducers/bookReducer';
 
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, setBooks] = useState([
-        {title: 'The power of positive thinking', author: 'Vincent Pill', id: 1},
-        {title: 'The power of habit', author: 'Charlse Duhig', id: 2},
-        {title: 'The power of of your mind', author: 'Dohig', id: 3}
-    ]);
-    const addNewBook = (title, author) => {
-        setBooks([...books, {title, author, id: uuid()}])
-    };
-    const removeBook = (id) => {
-        setBooks(books.filter(book => book.id !== id));
-    };
-
+    const [books, dispatch] = useReducer(bookReducer, [], () => {
+        const localData = localStorage.getItem('books');
+        return localData ? JSON.parse(localData) : [];
+    });
+    useEffect(() => {
+        localStorage.setItem('books', JSON.stringify(books))
+    }, [books]);
     return (
-        <BookContext.Provider value={{books, addNewBook, removeBook}}>
+        <BookContext.Provider value={{books, dispatch}}>
             { props.children }
         </BookContext.Provider>
     );
